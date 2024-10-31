a9fad0965a7d2776224240711fb84173e095a273
Felix Marvin <felixmarvinonline@gmail.com>
2024-10-31 09:56:15 +0300

    integrate api for quote fetching

--- src/QuoteGenerator.js
+++ src/QuoteGenerator.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from "axios";
 
 const QuoteGenerator = () => {
-    const [quote, setQuote] = React.useState("");
-
+    const [quote, setQuote] = React.useState({});
+    const [loading, setLoading] = React.useState(false);
 
     const fetchQuote = async () => {
-        const response = await fetch("https://api.quotable.io/random");
-        const data = await response.json();
-        setQuote(data.content);
+        setLoading(true);
+        try {
+            const response = await axios.get("http://api.quotable.io/random");
+            const {data} = response;
+            setQuote(data);
+        } catch (error) {
+            console.error("Error fetching quote: ", error);
+        }finally {
+            setLoading(false);
+        }
     }
 
+    useEffect(() => {
+        fetchQuote().then(() => {
+            console.log("Quote fetched");
+        });
+
+
+        const intervalId = setInterval(async () => {
+            await fetchQuote();
+        }, 10000);
+
+        return () => clearInterval(intervalId);
+
+    }, []);
+
+
+    const handleClick = async () => {
+        await fetchQuote();
+    }
 
 
     return (
         <div className={"quote-container"}>
             <h1>Quote Generator</h1>
-            <p className={"quote-text"}>Quote Text</p>
-            <p className={"quote-author"}>Quote Author</p>
-            <button className={"btn"}>Generate Quote</button>
+            {loading ? (
+                <p className={"loading"}>Loading...</p>
+            ) : (
+                <>
+                <p className={"quote-text"}>{quote?.content}</p>
+                    <p className={"quote-author"}>{quote?.author}</p>
+                </>
+            )}
+            <button className={"btn"} onClick={handleClick} disabled={loading}>
+                {loading ? "Loading..." : "Generate Quote"}
+            </button>
         </div>
     );
 };
